5be15828e9e9f4a6b016a7ee321120655915494c
Nina Alger <nina.alger@gmail.com>
2026-03-03 15:28:28 +0100

    add weave.py file & upgrade pyproject.toml file

---
+++ weave_KG2BM.py
@@ -0,0 +1,13 @@
+import biocypher
+import ontoweaver
+
+# Define the path to the mapping file. This time we define two paris of DATABASE:MAPPING_FILE mappings.
+data_mappings = {f"./data/oncokb_biomarker_drug_associations.tsv": f"./KG2BM/adapters/oncoKB.yaml"}
+#                 f"./data/rna_tissue_consensus_head10.tsv": f"./KG2BM/adapters/HPA.yaml" }
+#                 f"./data/jobim_subsets/4_Fusion/cna_subset.csv": f"./jobim/4_Fusion/cna.yaml"}
+
+# Extract nodes and edges from the mapping file. Reconciliate properties, and write nodes.
+ontoweaver.weave(filename_to_mapping=data_mappings, 
+                 affix = "suffix",
+                 biocypher_config_path=f"./config/biocypher_config.yaml",
+                 schema_path=f"./config/schema_config.yaml",)
